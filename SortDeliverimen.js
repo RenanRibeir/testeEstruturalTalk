112883b37326b4d640773cc200807c34f0f430c0
Renan Ribeiro <renanribeiro@bee.com.br>
2024-05-28 00:53:33 -0300

    coverage final step

--- SortDeliverimen.js
+++ SortDeliverimen.js
@@ -1,41 +1,39 @@
-function sortByDesc(key) {
-  return (current, previous) => {
-    if (current[key] > previous[key]) {
-      return -1;
-    } else if (current[key] < previous[key]) {
-      return 1;
-    } else {
-      return 0;
-    }
-  };
-}
-
-function sortByAsc(key) {
-  return (current, previous) => {
-    if (current[key] < previous[key]) {
-      return -1;
-    } else if (current[key] > previous[key]) {
-      return 1;
-    } else {
-      return 0;
-    }
-  };
-}
-
 class SortDeliverymen {
+  sortByDesc(key) {
+    return (current, previous) => {
+      if (current[key] > previous[key]) {
+        return -1;
+      } else if (current[key] < previous[key]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+  }
+
+  sortByAsc(key) {
+    return (current, previous) => {
+      if (current[key] < previous[key]) {
+        return -1;
+      } else if (current[key] > previous[key]) {
+        return 1;
+      } else {
+        return 0;
+      }
+    };
+  }
+
   handleSort(merchant, deliverymen) {
-    if (merchant.type === "lunchbox" && merchant.locale === "Brasilia") {
-      const a = deliverymen.sort(sortByDesc("level"));
+    if (merchant.type === "lunchbox" && merchant.locale === "Brasilia" || merchant.type === "jewelry") {
+      return deliverymen.sort(this.sortByDesc("level"));
+    }
 
-      return a;
-    } else if (merchant.type === "lunchbox") {
-      const a = deliverymen.sort(sortByAsc("avgDeliveryTime"));
-
-      return a;
-    } else if (merchant.type === "jewelry") {
-      return deliverymen.sort(sortByDesc("level"));
-    } else if (merchant.type === "drugstore") {
-      return deliverymen.sort(sortByAsc("averageDeliveryCost"));
+    if (merchant.type === "lunchbox") {
+      return deliverymen.sort(this.sortByAsc("avgDeliveryTime"));
+    }
+    
+    if (merchant.type === "drugstore") {
+      return deliverymen.sort(this.sortByAsc("averageDeliveryCost"));
     }
   }
 }
--- SortDeliverimen.test.js
+++ SortDeliverimen.test.js
@@ -56,59 +56,89 @@ const merchants = [
   },
 ];
 
-test("Lunchbox from Brasilia merchant", () => {
-  const sortedArray = new SortDeliverymen();
-  //10 7 6 2
-  const expectedArray = [Linus, Marie, John, Bob];
+const originalSort = Array.prototype.sort;
 
-  compareByNames(
+describe("SortDeliverymen", () => {
+  beforeEach(() => {
+    // Limpa todos os mocks antes de cada teste
+    jest.resetAllMocks();
+    Array.prototype.sort = originalSort;
+  });
+
+  it("Lunchbox from Brasilia merchant order by level desc", () => {
+    Array.prototype.sort = jest.fn(() => {}); //consigo injetar um funcionamento até mesmo em funções nativas do javascript
+
+    const sortedArray = new SortDeliverymen();
+    const sortByDesc = jest
+      .spyOn(sortedArray, "sortByDesc")
+      .mockImplementation(() => {}); // consigo injetar um funcionamento em funções da minha classe
+
+    sortedArray.handleSort(merchants[2], []), //Não importa o array que eu passo mas sim o merchant por conta do comportamento manipulado
+      expect(sortByDesc).toHaveBeenCalledWith('level'); // posso me assegurar se o merchant tiver certas caracteristicas será ordenado por level
+  });
+
+  it("Lunchbox from Brasilia merchant order desc", () => {
+    const sortedArray = new SortDeliverymen();
+    const spy = jest.spyOn(sortedArray, "sortByDesc");
+
     sortedArray.handleSort(merchants[2], [John, Bob, Marie, Linus]),
-    expectedArray
-  );
+      expect(spy).toHaveBeenCalled();
+  });
+
+  it("Lunchbox from Brasilia merchant", () => {
+    const sortedArray = new SortDeliverymen();
+    //10 7 6 2
+    const expectedArray = [Linus, Marie, John, Bob];
+
+    compareByNames(
+      sortedArray.handleSort(merchants[2], [John, Bob, Marie, Linus]),
+      expectedArray
+    );
+  });
+
+  it("Lunchbox merchant", () => {
+    const sortedArray = new SortDeliverymen();
+    //15 20 20 30
+    const expectedArray = [John, Marie, Linus, Bob];
+
+    compareByNames(
+      sortedArray.handleSort(merchants[1], [John, Bob, Marie, Linus]),
+      expectedArray
+    );
+  });
+
+  it("Jewelry merchant", () => {
+    const sortedArray = new SortDeliverymen();
+    //10 7 6 2
+    const expectedArray = [Linus, Marie, John, Bob];
+
+    compareByNames(
+      sortedArray.handleSort(merchants[3], [John, Bob, Marie, Linus]),
+      expectedArray
+    );
+  });
+
+  it("Drugstore merchant", () => {
+    const sortDeliverymen = new SortDeliverymen();
+    // 20 20 25 30
+    const expectedArray = [John, Bob, Marie, Linus];
+    const sortedArray = sortDeliverymen.handleSort(merchants[0], [
+      John,
+      Bob,
+      Marie,
+      Linus,
+    ]);
+
+    compareByNames(sortedArray, expectedArray);
+  });
+
+  function compareByNames(sortedArray, expectedArray) {
+    const sortedNames = sortedArray.map((current) => current.name);
+    const expectedNames = expectedArray.map((current) => current.name);
+
+    // console.log("expected: ", expectedNames);
+    // console.log("sorted: ", sortedNames);
+
+    expect(sortedNames).toEqual(expectedNames);
+  }
 });
-
-test("Lunchbox merchant", () => {
-  const sortedArray = new SortDeliverymen();
-  //15 20 20 30
-  const expectedArray = [John, Marie, Linus, Bob];
-
-  compareByNames(
-    sortedArray.handleSort(merchants[1], [John, Bob, Marie, Linus]),
-    expectedArray
-  );
-});
-
-test("Jewelry merchant", () => {
-  const sortedArray = new SortDeliverymen();
-  //10 7 6 2
-  const expectedArray = [Linus, Marie, John, Bob];
-
-  compareByNames(
-    sortedArray.handleSort(merchants[3], [John, Bob, Marie, Linus]),
-    expectedArray
-  );
-});
-
-test("Drugstore merchant", () => {
-  const sortDeliverymen = new SortDeliverymen();
-  // 20 20 25 30
-  const expectedArray = [John, Bob, Marie, Linus];
-  const sortedArray = sortDeliverymen.handleSort(merchants[0], [
-    John,
-    Bob,
-    Marie,
-    Linus,
-  ]);
-
-  compareByNames(sortedArray, expectedArray);
-});
-
-function compareByNames(sortedArray, expectedArray) {
-  const sortedNames = sortedArray.map((current) => current.name);
-  const expectedNames = expectedArray.map((current) => current.name);
-
-  console.log("expected: ", expectedNames);
-  console.log("sorted: ", sortedNames);
-
-  expect(sortedNames).toEqual(expectedNames);
-}
